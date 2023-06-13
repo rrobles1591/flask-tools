2d180bdd545c5696573e3b870fd03672067809a6
root <root@Phoebe.localdomain>
2023-06-12 21:04:23 -0400

    uno

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+RESPONSES_KEY = "responses"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "query"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def first_page():
+    return render_template("survey_start.html", survey=survey)
+
+@app.route("/begin", methods=["POST"])
+def responses():
+
+    session[RESPONSES_KEY] = []
+    return redirect("/questions/0")
+
+@app.route("/questions/<int:qid>")
+def show_questions(qid):
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        return redirect("/")
+    
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    
+    if (len(responses)) != qid:
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+    
+    question = survey.questions[qid]
+    return render_template(
+        "questions.html", question_num=qid, question=question)
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    choice = request.form['answer']
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.route("/complete")
+def complete():
+    """Survey complete. Show completion page."""
+
+    return render_template("completion.html")
